162f540990ba5df02a0687a2340e67cb19eeca9b
Xianhua Liu <xl24@duke.edu>
2007-09-06 13:47:13 +0000

    added function to handle the mouse events occurred on the table cell for adding new attribute.

--- WebRoot/jsp/editnow.js
+++ WebRoot/jsp/editnow.js
@@ -15,6 +15,8 @@ function onValueMouseOver(id)
 {
 	var nm=document.getElementById(id);
 	nm.style.border="1px solid #445";
+	
+	
 }
 
 function onValueMouseOut(id)
@@ -53,7 +55,60 @@ function createElement(id,field_name,value)
 			el.options[i]=new Option(values[i],values[i],defaultSelected, selected);
 			if(selected)el.options[i].setAttribute("selected","yes");
 		}
+		el.options[i]=new Option("","",false,false);
 	}
 	
 	return el;
+}
+
+function onFieldChange()
+{
+	
+	var id=-1;
+	var oField=document.getElementById(id);
+	var nm=document.getElementById(-2);
+	var field_name=oField.value;
+	var el=createElement(-2,field_name,"");
+	nm.parentNode.replaceChild(el,nm);
+	el.focus();
+	
+}
+function onFieldClicked(id, namespace)
+{
+	var nm=document.getElementById(id);
+	el=document.createElement("select");
+	el.setAttribute("name",id);
+	el.setAttribute("id",id);
+	
+	if (el.addEventListener) {
+		el.addEventListener ("change",onFieldChange,false);
+	} else if (el.attachEvent) {
+		el.attachEvent ("onchange",onFieldChange);
+	} else {
+		el.onchange = onFieldChange;
+	}
+
+	for(var i=0;i<attrs.length;i++)
+	{
+		el.options[i]=new Option(attrs[i],attrs[i],false, false);
+	}
+	
+	nm.parentNode.replaceChild(el,nm);
+
+	el.focus();
+
+}
+
+function onFieldMouseOver(id)
+{
+	var nm=document.getElementById(id);
+	nm.style.border="1px solid #445";
+	
+	
+}
+
+function onFieldMouseOut(id)
+{
+	var nm=document.getElementById(id);
+	nm.style.border="0px solid #445";
 }
