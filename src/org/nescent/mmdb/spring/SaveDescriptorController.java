66fb68527ea643bebc30ec3b1ffcb78508c0fb2a
Xianhua Liu <xl24@duke.edu>
2007-09-19 17:46:10 +0000

    Changed to save descriptor and its attributes in one controller.

--- src/org/nescent/mmdb/spring/SaveDescriptorController.java
+++ src/org/nescent/mmdb/spring/SaveDescriptorController.java
@@ -24,6 +24,8 @@ import org.nescent.mmdb.hibernate.dao.MmSpeciesAttrCvtermAssoc;
 import org.nescent.mmdb.hibernate.dao.MmSpeciesAttrCvtermAssocDAO;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+import org.nescent.mmdb.util.Fields;
+import org.nescent.mmdb.util.RetrieveData;
 
 public class SaveDescriptorController implements Controller {
 
@@ -40,99 +42,133 @@ public class SaveDescriptorController implements Controller {
 			throw new Exception("No species discriptor found.");
 		}
 		
-		MmSpeciesAttrCvtermAssocDAO assocDao=new MmSpeciesAttrCvtermAssocDAO();
-		
 		Session sess=HibernateSessionFactory.getSession();
 		Transaction tr=sess.beginTransaction();
 		
 		try
 		{
-			for(Enumeration en=arg0.getParameterNames();en.hasMoreElements();)
+			saveSpeciesReference(study,arg0,sess);
+			saveAttributes(study,arg0,sess);
+			sess.flush();
+			tr.commit();
+			RetrieveData.retrieveDiscriptor(study);
+			return new ModelAndView("descriptor","descriptor",study);
+		}
+		catch(Exception e){
+			throw e;
+		}
+		finally{
+			if(tr!=null)
 			{
-				String cvid=(String)en.nextElement();
-				if(cvid.equals("id") ||cvid.equals("-2") ) continue;
-				else if(cvid.equals("-1"))
+				tr.rollback();
+			}
+			sess.close();
+		}
+	}
+	
+	public void saveAttributes(MmMatingSystemStudy study, HttpServletRequest arg0, Session sess) throws Exception
+	{
+		String id=arg0.getParameter("id");
+		MmSpeciesAttrCvtermAssocDAO assocDao=new MmSpeciesAttrCvtermAssocDAO();
+		
+		
+		for(Enumeration en=arg0.getParameterNames();en.hasMoreElements();)
+		{
+			String cvid=(String)en.nextElement();
+			
+			if(cvid.equals("id") || cvid.equals("-2")|| (cvid.indexOf("-")!=-1 && ! cvid.equals("-1")) ) continue;
+			else if(cvid.equals("-1"))
+			{
+				String field=arg0.getParameter("-1");
+				String value=arg0.getParameter("-2");
+				if(field!=null && value!=null)
 				{
-					String field=arg0.getParameter("-1");
-					String value=arg0.getParameter("-2");
-					if(field!=null && value!=null)
+					MmCvTermDAO termDao=new MmCvTermDAO();
+					List terms=termDao.findByName(field);
+					if(terms.size()>0)
 					{
-						MmCvTermDAO termDao=new MmCvTermDAO();
-						List terms=termDao.findByName(field);
-						if(terms.size()>0)
-						{
-							MmCvTerm term=(MmCvTerm)terms.toArray()[0];
-							MmSpeciesAttrCvtermAssoc assoc=new MmSpeciesAttrCvtermAssoc();
-							assoc.setMmCvTerm(term);
-							assoc.setMmMatingSystemStudy(study);
-							assoc.setValue(value);
-							term.getMmSpeciesAttrCvtermAssocs().add(assoc);
-							study.getMmSpeciesAttrCvtermAssocs().add(assoc);
-							sess.save(assoc);
-						}
-					}
-				}
-				else
-				{
-					MmSpeciesAttrCvtermAssoc assoc=assocDao.findById(Integer.valueOf(cvid));
-					if(assoc==null)
-						throw new Exception("No MmSpeciesAttrCvtermAssoc found with the id: "+id);
-					
-					String value=arg0.getParameter(cvid);
-					if(value==null || value.equals(""))
-					{
-						sess.delete(assoc);
-					}
-					else
-					{
+						MmCvTerm term=(MmCvTerm)terms.toArray()[0];
+						MmSpeciesAttrCvtermAssoc assoc=new MmSpeciesAttrCvtermAssoc();
+						assoc.setMmCvTerm(term);
+						assoc.setMmMatingSystemStudy(study);
 						assoc.setValue(value);
-						sess.update(assoc);
+						term.getMmSpeciesAttrCvtermAssocs().add(assoc);
+						study.getMmSpeciesAttrCvtermAssocs().add(assoc);
+						sess.save(assoc);
 					}
 				}
 			}
-			sess.flush();
-			tr.commit();
-			retrieveDiscriptor(study);
-			return new ModelAndView("descriptor","descriptor",study);
+			else
+			{
+				MmSpeciesAttrCvtermAssoc assoc=assocDao.findById(Integer.valueOf(cvid));
+				if(assoc==null)
+					throw new Exception("No MmSpeciesAttrCvtermAssoc found with the id: "+id);
+				
+				String value=arg0.getParameter(cvid);
+				if(value==null || value.equals(""))
+				{
+					sess.delete(assoc);
+				}
+				else
+				{
+					assoc.setValue(value);
+					sess.update(assoc);
+				}
+			}
 		}
-		catch(Exception e)
-		{
-			throw e;
-		}
-		finally
-		{
-			tr.rollback();
-			sess.close();
-		}
-		
-	}
 	
-	public void retrieveDiscriptor(MmMatingSystemStudy mmStudy) throws Exception
-	{
-		MmSpecies species=mmStudy.getMmSpecies();
-		species.getFamily();
-		species.getGenus();
-		species.getSpecies();
-		mmStudy.getLatitude();
-		MmReferencePart refPart=mmStudy.getMmReferencePart();
-		refPart.getName();
-		MmReference ref=refPart.getMmReference();
-		ref.getCitation();
-		ref.getFullReference();
-		
-		Set mmCvTermsAssocs=mmStudy.getMmSpeciesAttrCvtermAssocs();
-		
-		for(Iterator it=mmCvTermsAssocs.iterator();it.hasNext();)
-		{
-			MmSpeciesAttrCvtermAssoc cvAssoc=(MmSpeciesAttrCvtermAssoc)it.next();
-			MmCvTerm term=cvAssoc.getMmCvTerm();
-			term.getCvtermOid();
-			term.getDescription();
-			term.getName();
-			term.getNamespace();
-			term.getValueType();
-			cvAssoc.getValue();
-		}
 	}
-
+	public void saveSpeciesReference(MmMatingSystemStudy study,HttpServletRequest arg0, Session sess) throws Exception
+	{
+		String family=arg0.getParameter(Fields.getFieldId("Family"));
+		String genus=arg0.getParameter(Fields.getFieldId("genus"));
+		String species=arg0.getParameter(Fields.getFieldId("species"));
+		String citation=arg0.getParameter(Fields.getFieldId("citation"));
+		String full=arg0.getParameter(Fields.getFieldId("fullreference"));
+		String part=arg0.getParameter(Fields.getFieldId("part"));
+		String latitude=arg0.getParameter(Fields.getFieldId("latitude"));
+		
+		if(genus!=null && genus.trim().equals(""))
+			throw new  Exception("Empty genus.");
+		if(species!=null && species.trim().equals(""))
+			throw new  Exception("Empty species.");
+		if(citation!=null && citation.trim().equals(""))
+			throw new  Exception("Empty citation.");	
+		
+		
+		
+		//get or create species
+		MmSpecies sp=study.getMmSpecies();
+		
+		if(sp==null)
+			throw new Exception("No species found for the mating system study.");
+		
+		if(family!=null)
+			sp.setFamily(family);
+		if(genus!=null)	
+			sp.setGenus(genus);
+		if(species!=null)
+		sp.setSpecies(species);
+		
+		if(latitude!=null)				
+			study.setLatitude(latitude);
+		
+		
+		MmReferencePart mpart=study.getMmReferencePart();
+		if(mpart!=null)
+		{
+			if(part!=null)
+				mpart.setName(part);
+			MmReference ref=mpart.getMmReference();
+			if(ref!=null)
+			{
+				if(citation!=null)
+					ref.setCitation(citation);
+				if(full!=null)	
+					ref.setFullReference(full);
+			}
+		}
+		
+		sess.update(study);
+	}
 }
