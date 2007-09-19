af637a9abc3c2be3e996c12c22521e8ce0b636ea
Xianhua Liu <xl24@duke.edu>
2007-09-19 17:49:11 +0000

    used static function of RetrieveData class to retrieve associate data objects of experimental study before closing the hibernate session.

--- src/org/nescent/mmdb/spring/SavePopulationController.java
+++ src/org/nescent/mmdb/spring/SavePopulationController.java
@@ -29,6 +29,7 @@ import org.nescent.mmdb.hibernate.dao.MmReference;
 import org.nescent.mmdb.hibernate.dao.MmReferencePart;
 import org.nescent.mmdb.hibernate.dao.MmSpecies;
 import org.nescent.mmdb.util.Fields;
+import org.nescent.mmdb.util.RetrieveData;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -152,7 +153,7 @@ public class SavePopulationController implements Controller {
             sess.update(sample);
             sess.flush();
             tr.commit();
-            retrieveEnvironmentalStudy(study);
+            RetrieveData.retrieveEnvironmentalStudy(study);
             Map model=new HashMap();
 	        model.put("envstudy",study);
 	        model.put("tab","population");
@@ -170,85 +171,5 @@ public class SavePopulationController implements Controller {
         }
         
     }
-    
-    public void retrieveEnvironmentalStudy(MmExperimentStudy envStudy) throws Exception
-    {
-        MmPopulationSample sample=envStudy.getMmPopulationSample();
-        if(sample!=null)
-        {
-            
-            MmSpecies species=sample.getMmSpecies();
-            species.getFamily();
-            species.getGenus();
-            species.getSpecies();
-            
-            sample.getComments();
-            sample.getEnvironment();
-            sample.getGeographicLocation();
-            Set set =sample.getMmPopSampleAttrCvtermAssocs();
-            for(Iterator it=set.iterator();it.hasNext();)
-            {
-                MmPopSampleAttrCvtermAssoc cvAssoc=(MmPopSampleAttrCvtermAssoc)it.next();
-                MmCvTerm term=cvAssoc.getMmCvTerm();
-                term.getCvtermOid();
-                term.getDescription();
-                term.getName();
-                term.getNamespace();
-                term.getValueType();
-                cvAssoc.getValue();
-            }
-            
-            sample.getName();
-            sample.getPopulation();
-            sample.getYear();
-            sample.getPopulationSampleOid();
-        }
-        
-        envStudy.getExperimentStudyOid();
-        Set set= envStudy.getMmDataRecords();
-        for(Iterator it=set.iterator();it.hasNext();)
-        {
-            MmDataRecord record=(MmDataRecord)it.next();
-            record.getName();
-            record.getOutCrossingStdDev();
-            record.getOutCrossingValue();
-            record.getSelfingStdDev();
-            record.getSelfingValue();
-            record.getType();
-        }
-        
-        MmDevelopmentalStage stage=envStudy.getMmDevelopmentalStage();
-        stage.getName();
-        
-        set= envStudy.getMmExperimentValues();
-        for(Iterator it=set.iterator();it.hasNext();)
-        {
-            MmExperimentValue value=(MmExperimentValue)it.next();
-            
-            MmCvTerm term=value.getMmCvTerm();
-            term.getCvtermOid();
-            term.getDescription();
-            term.getName();
-            term.getNamespace();
-            term.getValueType();
-            
-            value.getValue();
-        }
-        
-        
-        
-        MmReferencePart refPart=envStudy.getMmReferencePart();
-        if(refPart!=null)
-        {
-            refPart.getName();
-            MmReference ref=refPart.getMmReference();
-            if(ref!=null)
-            {
-                ref.getCitation();
-                ref.getFullReference();
-            }
-        }
-		
-	}
-
+   
 }
