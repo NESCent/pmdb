a8fda5911af8a3aa610917a0912c4ea834acccc8
Xianhua Liu <xl24@duke.edu>
2007-09-27 15:10:30 +0000

    returned statistical data of studies

--- src/org/nescent/mmdb/spring/SearchStudyController.java
+++ src/org/nescent/mmdb/spring/SearchStudyController.java
@@ -1,5 +1,6 @@
 package org.nescent.mmdb.spring;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -7,7 +8,11 @@ import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.nescent.mmdb.hibernate.HibernateSessionFactory;
+import org.nescent.mmdb.hibernate.dao.MmCvTerm;
 import org.nescent.mmdb.hibernate.dao.MmExperimentStudy;
 import org.nescent.mmdb.hibernate.dao.MmMatingSystemStudy;
 import org.nescent.mmdb.hibernate.dao.MmPopulationSample;
@@ -18,6 +23,7 @@ import org.nescent.mmdb.hibernate.dao.MmSpeciesDAO;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 import org.nescent.mmdb.util.RetrieveData;
+import org.nescent.mmdb.util.Species;
 
 public class SearchStudyController implements Controller {
 
@@ -35,19 +41,61 @@ public class SearchStudyController implements Controller {
 		
 		MmSpeciesDAO spDao=new MmSpeciesDAO();
 		List specieses = spDao.findByName(familyName,genusName,speciesName);
-		if(specieses.size()!=1)
+		List sps=new ArrayList();
+		
+		for(int i=0;i<specieses.size();i++)
 		{
-			HibernateSessionFactory.closeSession();
-			return new ModelAndView("speciesList","list",specieses);
-		}
-		else
-		{
-			MmSpecies species=(MmSpecies)specieses.get(0);
-			RetrieveData.retrieveSpecies(species);
-			HibernateSessionFactory.closeSession();
-			return new ModelAndView("studies","species",species);
+			MmSpecies species=(MmSpecies)specieses.get(i);
+			sps.add(getSpecies(species));
 		}
 		
+		HibernateSessionFactory.closeSession();
+		return new ModelAndView("speciesList","list",sps);
+
+	}
+	
+	public Species getSpecies(MmSpecies species){
+		Species sp=new Species();
+		sp.setFamily(species.getFamily());
+		sp.setGenus(species.getGenus());
+		sp.setSpecies(species.getSpecies());
+		sp.setId(species.getSpeciesOid());
+		int inbreedNum=0;
+		int outNum=0;
+		int pollNum=0;
+		sp.setPopulationNum(species.getMmPopulationSamples().size());
 		
+		Session sess=HibernateSessionFactory.getSession();
+		String hql="SELECT  ";
+		hql+= "count(study),study.mmCvTerm.name FROM MmExperimentStudy study ";
+		hql+=" INNER JOIN study.mmPopulationSample.mmSpecies species";
+		hql+=" WHERE species.species='"+sp.getSpecies()+"'";
+		hql+=" AND species.genus='"+ sp.getGenus()+"'";
+		hql+=" AND species.family='"+sp.getFamily()+"'";
+		hql+=" GROUP BY study.mmCvTerm.name ";
+		//hql+=" having species.species='latifolia'";
+			
+		Query q=sess.createQuery(hql);
+		List list=q.list();
+		for(int i=0;i<list.size();i++)
+		{
+			
+			Object [] objs=(Object [])list.get(i);
+			
+			String studyName=(String)objs[1];
+			if(studyName.indexOf("inbreed")!=-1)
+				inbreedNum=((Integer)objs[0]).intValue();
+			else if(studyName.indexOf("outcross")!=-1)
+				outNum=((Integer)objs[0]).intValue();
+			else if(studyName.indexOf("pollination")!=-1)
+				pollNum=((Integer)objs[0]).intValue();
+			
+		}
+		
+		sp.setInbreedNum(inbreedNum);
+		sp.setOutcrossingNum(outNum);
+		sp.setPollinationNum(pollNum);
+		
+		return sp;
 	}
 }
