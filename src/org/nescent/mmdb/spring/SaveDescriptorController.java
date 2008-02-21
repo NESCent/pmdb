a3d528ca951510de45d62aeaa7f28818ea8161b7
Xianhua Liu <xl24@duke.edu>
2008-02-21 21:42:47 +0000

    changed to use ordinary form instead of that with editable input field.

--- src/org/nescent/mmdb/spring/SaveDescriptorController.java
+++ src/org/nescent/mmdb/spring/SaveDescriptorController.java
@@ -2,173 +2,216 @@ package org.nescent.mmdb.spring;
 
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.nescent.mmdb.hibernate.HibernateSessionFactory;
 import org.nescent.mmdb.hibernate.dao.MmCvTerm;
 import org.nescent.mmdb.hibernate.dao.MmCvTermDAO;
 import org.nescent.mmdb.hibernate.dao.MmMatingSystemStudy;
-import org.nescent.mmdb.hibernate.dao.MmMatingSystemStudyDAO;
 import org.nescent.mmdb.hibernate.dao.MmReference;
 import org.nescent.mmdb.hibernate.dao.MmReferencePart;
 import org.nescent.mmdb.hibernate.dao.MmSpecies;
 import org.nescent.mmdb.hibernate.dao.MmSpeciesAttrCvtermAssoc;
-import org.nescent.mmdb.hibernate.dao.MmSpeciesAttrCvtermAssocDAO;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
-import org.nescent.mmdb.util.Fields;
-import org.nescent.mmdb.util.RetrieveData;
 
 public class SaveDescriptorController implements Controller {
+    private static Logger log;
 
-	public ModelAndView handleRequest(HttpServletRequest arg0,
-			HttpServletResponse arg1) throws Exception {
-		
-		String id=arg0.getParameter("id");
-		if(id==null)
-			throw new Exception("No id specified.");
-		MmMatingSystemStudyDAO mmStDao=new MmMatingSystemStudyDAO();
-		MmMatingSystemStudy study = mmStDao.findById(Integer.valueOf(id));
-		if(study==null)
-		{
-			throw new Exception("No species discriptor found.");
-		}
-		
-		Session sess=HibernateSessionFactory.getSession();
-		Transaction tr=sess.beginTransaction();
-		
-		try
-		{
-			saveSpeciesReference(study,arg0,sess);
-			saveAttributes(study,arg0,sess);
-			sess.flush();
-			tr.commit();
-			RetrieveData.retrieveDiscriptor(study);
-			return new ModelAndView("descriptor","descriptor",study);
-		}
-		catch(Exception e){
-			throw e;
-		}
-		finally{
-			if(tr!=null)
-			{
-				tr.rollback();
-			}
-			sess.close();
-		}
+    private static Logger log() {
+	if (log == null) {
+	    log = Logger.getLogger(SaveDescriptorController.class);
 	}
-	
-	public void saveAttributes(MmMatingSystemStudy study, HttpServletRequest arg0, Session sess) throws Exception
-	{
-		String id=arg0.getParameter("id");
-		MmSpeciesAttrCvtermAssocDAO assocDao=new MmSpeciesAttrCvtermAssocDAO();
-		
-		
-		for(Enumeration en=arg0.getParameterNames();en.hasMoreElements();)
-		{
-			String cvid=(String)en.nextElement();
-			
-			if(cvid.equals("id") || cvid.equals("-2")|| (cvid.indexOf("-")!=-1 && ! cvid.equals("-1")) ) continue;
-			else if(cvid.equals("-1"))
-			{
-				String field=arg0.getParameter("-1");
-				String value=arg0.getParameter("-2");
-				if(field!=null && value!=null)
-				{
-					MmCvTermDAO termDao=new MmCvTermDAO();
-					List terms=termDao.findByName(field);
-					if(terms.size()>0)
-					{
-						MmCvTerm term=(MmCvTerm)terms.toArray()[0];
-						MmSpeciesAttrCvtermAssoc assoc=new MmSpeciesAttrCvtermAssoc();
-						assoc.setMmCvTerm(term);
-						assoc.setMmMatingSystemStudy(study);
-						assoc.setValue(value);
-						term.getMmSpeciesAttrCvtermAssocs().add(assoc);
-						study.getMmSpeciesAttrCvtermAssocs().add(assoc);
-						sess.save(assoc);
-					}
-				}
-			}
-			else
-			{
-				MmSpeciesAttrCvtermAssoc assoc=assocDao.findById(Integer.valueOf(cvid));
-				if(assoc==null)
-					throw new Exception("No MmSpeciesAttrCvtermAssoc found with the id: "+id);
-				
-				String value=arg0.getParameter(cvid);
-				if(value==null || value.equals(""))
-				{
-					sess.delete(assoc);
-				}
-				else
-				{
-					assoc.setValue(value);
-					sess.update(assoc);
-				}
-			}
-		}
-	
+	return log;
+    }
+
+    public ModelAndView handleRequest(HttpServletRequest request,
+	    HttpServletResponse response) {
+
+	String id = nullIfEmpty(request.getParameter("id"));
+	if (id == null) {
+	    log().error("no id specified");
+	    throw new IllegalArgumentException("no id specified");
 	}
-	public void saveSpeciesReference(MmMatingSystemStudy study,HttpServletRequest arg0, Session sess) throws Exception
-	{
-		String family=arg0.getParameter(Fields.getFieldId("Family"));
-		String genus=arg0.getParameter(Fields.getFieldId("genus"));
-		String species=arg0.getParameter(Fields.getFieldId("species"));
-		String citation=arg0.getParameter(Fields.getFieldId("citation"));
-		String full=arg0.getParameter(Fields.getFieldId("fullreference"));
-		String part=arg0.getParameter(Fields.getFieldId("part"));
-		String latitude=arg0.getParameter(Fields.getFieldId("latitude"));
-		
-		if(genus!=null && genus.trim().equals(""))
-			throw new  Exception("Empty genus.");
-		if(species!=null && species.trim().equals(""))
-			throw new  Exception("Empty species.");
-		if(citation!=null && citation.trim().equals(""))
-			throw new  Exception("Empty citation.");	
-		
-		
-		
-		//get or create species
-		MmSpecies sp=study.getMmSpecies();
-		
-		if(sp==null)
-			throw new Exception("No species found for the mating system study.");
-		
-		if(family!=null)
-			sp.setFamily(family);
-		if(genus!=null)	
-			sp.setGenus(genus);
-		if(species!=null)
-		sp.setSpecies(species);
-		
-		if(latitude!=null)				
-			study.setLatitude(latitude);
-		
-		
-		MmReferencePart mpart=study.getMmReferencePart();
-		if(mpart!=null)
-		{
-			if(part!=null)
-				mpart.setName(part);
-			MmReference ref=mpart.getMmReference();
-			if(ref!=null)
-			{
-				if(citation!=null)
-					ref.setCitation(citation);
-				if(full!=null)	
-					ref.setFullReference(full);
-			}
-		}
-		
-		sess.update(study);
+
+	Session session = HibernateSessionFactory.getSession();
+	Transaction tx = session.beginTransaction();
+
+	try {
+	    MmMatingSystemStudy study = (MmMatingSystemStudy) session.get(
+		    MmMatingSystemStudy.class, Integer.valueOf(id));
+	    if (study == null) {
+		log().error(
+			"failed to retrieve the species descriptor with id: "
+				+ id);
+		throw new IllegalArgumentException(
+			"failed to retrieve the species descriptor with id: "
+				+ id);
+	    }
+	    saveSpeciesReference(study, request, session);
+	    saveAttributes(study, request, session);
+
+	    session.flush();
+	    tx.commit();
+
+	    String sql = "FROM MmCvTerm term where term.namespace='species_attribute' order by term.name";
+	    Query q = session.createQuery(sql);
+	    Map<String, Object> model = new HashMap<String, Object>();
+	    model.put("study", study);
+	    model.put("species_attributes", q.list());
+	    model.put("tab", "species");
+	    model.put("message", "Seccessfully updated.");
+	    return new ModelAndView("editSpeciesDescriptor", model);
+
+	} catch (NumberFormatException nfe) {
+	    log().error("invalid number: " + id, nfe);
+	    throw new IllegalArgumentException("invalid number: " + id, nfe);
+	} catch (HibernateException he) {
+	    log().error("filed to edit the species descriptor with id: " + id,
+		    he);
+	    throw he;
+
+	} finally {
+	    if (!tx.wasCommitted())
+		tx.rollback();
 	}
+    }
+
+    private String nullIfEmpty(String s) {
+	return (s == null || s.trim().equals("")) ? null : s;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void saveAttributes(MmMatingSystemStudy study,
+	    HttpServletRequest request, Session sess) {
+
+	for (Enumeration en = request.getParameterNames(); en.hasMoreElements();) {
+	    String cvid = (String) en.nextElement();
+
+	    if (cvid.indexOf("attr_") != -1) {
+		String ss[] = cvid.split("_");
+		if (ss.length != 2) {
+		    log().error("invalid attribute id: " + cvid);
+		}
+		String aid = ss[1];
+		String attr = nullIfEmpty(request.getParameter(cvid));
+		String attrValue = nullIfEmpty(request
+			.getParameter("attrValue_" + aid));
+
+		if (aid.equals("newattr")) {
+		    if (attrValue != null && attr != null) {
+			MmCvTermDAO termDao = new MmCvTermDAO();
+			List terms = termDao.findByName(attr);
+			if (terms.size() > 0) {
+			    MmCvTerm term = (MmCvTerm) terms.toArray()[0];
+			    MmSpeciesAttrCvtermAssoc assoc = new MmSpeciesAttrCvtermAssoc();
+			    assoc.setMmCvTerm(term);
+			    assoc.setMmMatingSystemStudy(study);
+			    assoc.setValue(attrValue);
+			    term.getMmSpeciesAttrCvtermAssocs().add(assoc);
+			    study.getMmSpeciesAttrCvtermAssocs().add(assoc);
+			    sess.save(assoc);
+			} else {
+			    log().error(
+				    "failed to find the cvterm for species attribute: "
+					    + attr);
+			    throw new IllegalArgumentException(
+				    "failed to find the cvterm for species attribute: "
+					    + attr);
+			}
+		    }
+		} else {
+
+		    MmSpeciesAttrCvtermAssoc assoc = null;
+		    for (Object o : study.getMmSpeciesAttrCvtermAssocs()) {
+			MmSpeciesAttrCvtermAssoc as = (MmSpeciesAttrCvtermAssoc) o;
+			if (as.getMsacaOid().equals(Integer.valueOf(aid))) {
+			    assoc = as;
+			    break;
+			}
+		    }
+		    if (assoc == null) {
+			log().error(
+				"failed to find the MmSpeciesAttrCvtermAssoc with the id: "
+					+ aid);
+			throw new IllegalArgumentException(
+				"failed to find the MmSpeciesAttrCvtermAssoc with the id: "
+					+ aid);
+		    }
+		    if (attrValue == null) {
+			sess.delete(assoc);
+			study.getMmSpeciesAttrCvtermAssocs().remove(assoc);
+		    } else {
+			assoc.setValue(attrValue);
+			sess.update(assoc);
+		    }
+		}
+
+	    }
+
+	}
+
+    }
+
+    public void saveSpeciesReference(MmMatingSystemStudy study,
+	    HttpServletRequest request, Session sess) {
+	String family = nullIfEmpty(request.getParameter("family"));
+	String genus = nullIfEmpty(request.getParameter("genus"));
+	String species = nullIfEmpty(request.getParameter("species"));
+	String citation = nullIfEmpty(request.getParameter("citation"));
+	String full = nullIfEmpty(request.getParameter("fullReference"));
+	String part = nullIfEmpty(request.getParameter("referencePart"));
+	String latitude = nullIfEmpty(request.getParameter("latitude"));
+
+	if (genus == null) {
+	    log().error("no genus specified");
+	    throw new IllegalArgumentException("no genus specified");
+	}
+	if (species == null) {
+	    log().error("no species specified");
+	    throw new IllegalArgumentException("no species specified");
+	}
+	if (citation == null) {
+	    log().error("no citation specified");
+	    throw new IllegalArgumentException("no citation specified");
+	}
+
+	// get or create species
+	MmSpecies sp = study.getMmSpecies();
+
+	if (sp == null) {
+	    log().error(
+		    "no species found for the mating system study: "
+			    + study.getMatingSystemStudyOid());
+	    throw new IllegalArgumentException(
+		    "no species found for the mating system study: "
+			    + study.getMatingSystemStudyOid());
+	}
+
+	sp.setFamily(family);
+	sp.setGenus(genus);
+	sp.setSpecies(species);
+	study.setLatitude(latitude);
+
+	MmReferencePart mpart = study.getMmReferencePart();
+	if (mpart != null) {
+	    mpart.setName(part);
+	    MmReference ref = mpart.getMmReference();
+	    if (ref != null) {
+		ref.setCitation(citation);
+		ref.setFullReference(full);
+	    }
+	}
+
+	sess.update(study);
+    }
 }
